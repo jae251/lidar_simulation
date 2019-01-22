ae2b7555bf66ea07e75b8d56e11bdc38bed902cc
Jan Esch <bosch.esch@extaccount.com>
2019-01-22 07:46:29 +0100

    function to load arbitrary obj files
    Branch: master

--- data_loaders/load_3d_models.py
+++ data_loaders/load_3d_models.py
@@ -1,16 +1,28 @@
 import numpy as np
+import os
 
 
-class WavefrontFileLoader:
-    def __init__(self, set_loader_mode):
-        raise NotImplementedError
-
-    def load_obj_file(self, filename):
-        raise NotImplementedError
+def load_obj_file(filename):
+    filename = os.path.expanduser(filename)
+    with open(filename) as f:
+        v, p = [], []
+        for line in f:
+            if line[:2] == "v ":
+                q = line.split()
+                r = (-float(q[3]), float(q[1]), float(q[2]))
+                v.append(r)
+            elif line[0] == "f":
+                q = [[int(i) for i in l.split("/")] for l in line.split()[1:]]
+                p.append(q)
+        vertices = np.array(v)
+        z_min = np.min(vertices[:, 2])
+        vertices[:, 2] -= z_min
+        polygons = np.array(p)[:, :, 0] - 1
+    return vertices, polygons
 
 
 def load_Porsche911():
-    file = "C:/Users/usr/Downloads/3d models/Porsche_911_GT2.obj"
+    file = os.path.expanduser("C:/Users/usr/Downloads/3d models/Porsche_911_GT2.obj")
     with open(file) as f:
         v, n, p = [], [], []
         for line in f:
