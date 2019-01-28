ace1c3553604b2687cfa01942c657c1f008bf37e
Jan Esch <bosch.esch@extaccount.com>
2019-01-28 14:15:40 +0100

    changed path, deleted code leftovers
    Branch: master

--- data_loaders/load_3d_models.py
+++ data_loaders/load_3d_models.py
@@ -22,7 +22,7 @@ def load_obj_file(filename):
 
 
 def load_Porsche911():
-    file = os.path.expanduser("C:/Users/usr/Downloads/3d models/Porsche_911_GT2.obj")
+    file = os.path.expanduser("~/Downloads/3d_models/Porsche_911_GT2.obj")
     with open(file) as f:
         v, n, p = [], [], []
         for line in f:
@@ -30,19 +30,13 @@ def load_Porsche911():
                 q = line.split()
                 r = (-float(q[3]), float(q[1]), float(q[2]))
                 v.append(r)
-            # elif line[:2] == "vn":
-            #     q = line.split()
-            #     r = (-float(q[3]), float(q[1]), float(q[2]))
-            #     n.append(r)
             elif line[0] == "f":
                 q = [[int(i) for i in l.split("/")] for l in line.split()[1:]]
                 p.append(q)
         vertices = np.array(v)
         z_min = np.min(vertices[:, 2])
         vertices[:, 2] -= z_min
-        # normals = np.array(n)
         polygons = np.array(p)[:, :, 0] - 1
-        # unused: vertex-normals <normals>, index info <polygons[:,:,1:]
     return vertices, polygons
 
 
