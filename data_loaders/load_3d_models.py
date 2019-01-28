fa196f48a9264262ba28f0085e726c38409cb8bf
Jan Esch <bosch.esch@extaccount.com>
2019-01-28 08:44:44 +0100

    Made import statement more general
    Branch: master

--- data_loaders/load_3d_models.py
+++ data_loaders/load_3d_models.py
@@ -48,7 +48,7 @@ def load_Porsche911():
 
 if __name__ == "__main__":
     vertices, polygons = load_Porsche911()
-    from utilities.visualization import visualize_2d
+    from lidar_simulation.utilities.visualization import visualize_2d
 
     visualize_2d(vertices)
     visualize_2d(vertices, plane=(0, 2))
